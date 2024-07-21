280ab3feeb693b3a7a8459a6d508269802d1dbeb
my 1st repository <supriyadhal50@gmail.com>
2024-07-21 12:47:29 +0530

    .

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -52,6 +52,7 @@ const userSchema = new Schema(
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
 
+  //auto generate the salt based on rounds(10) and hash
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
